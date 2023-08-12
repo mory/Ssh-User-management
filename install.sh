377a4616c9665e0ada2b3ba6c92e7426498badcc
Mory <819280+mory@users.noreply.github.com>
2023-08-12 18:18:00 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -195,7 +195,7 @@ sed -i 's/extension=intl/;extension=intl/' ${PHP_INI}
 fi
 bash <(curl -Ls https://raw.githubusercontent.com/mory/ioncube-loader/main/install.sh --ipv4)
 bash <(curl -Ls https://raw.githubusercontent.com/mory/Nethogs-Json/main/install.sh --ipv4)
-bash <(curl -Ls https://raw.githubusercontent.com/mory/Ssh-User-management/main/ssh-calls.sh --ipv4)
+#bash <(curl -Ls https://raw.githubusercontent.com/mory/Ssh-User-management/main/ssh-calls.sh --ipv4)
 mysql -e "create database ShaHaN;" &
 wait
 mysql -e "CREATE USER '${adminusername}'@'localhost' IDENTIFIED BY '${adminpassword}';" &
