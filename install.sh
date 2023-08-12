af12b5a8eb0288f9a2481459b7e4442d492368b1
Mory <819280+mory@users.noreply.github.com>
2023-08-12 18:34:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -226,7 +226,7 @@ crontab -l | grep -v 'p/killusers.sh'  | crontab  -
 * * * * * bash /var/www/html/p/killusers.sh >/dev/null 2>&1" ) | crontab - &
 wait
 sudo bash /var/www/html/p/ipv6.sh
-clear
+#clear
 printf "\nPanel Link : http://${ipv4}/p/index.php"
 printf "\nUserName : \e[31m${adminusername}\e[0m "
 printf "\nPassword : \e[31m${adminpassword}\e[0m "
